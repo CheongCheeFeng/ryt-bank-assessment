23367d16bb2526c12265c8a0ef4002710749f35d
cheefengcheong <cheefengcheong@gmail.com>
2025-05-10 12:47:12 +0800

    Add transaction screen

--- app/transactions-history.tsx
+++ app/transactions-history.tsx
@@ -1,24 +1,77 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-import { useNavigation } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { SectionList, StyleSheet, Text, View } from "react-native";
 
-const TransactionHistory = () => {
-  const navigation = useNavigation();
+import TransactionItem from "@/components/transaction-item";
+import { useUpdateHeaderOption } from "@/hooks/useUpdateHeaderOption";
+import { TransactionService } from "@/services/transaction.service";
+import { GroupedTransactions } from "@/utils/types";
 
+const TransactionHistoryScreen = () => {
+  useUpdateHeaderOption("Transaction History");
+
+  const router = useRouter();
+
+  const [recentTransactions, setRecentTransactions] = useState<
+    GroupedTransactions[]
+  >([]);
+
+  const fetchRecentTransactions = () => {
+    const transactions = TransactionService.getTransactionGroupByDate();
+    setRecentTransactions(transactions);
+  };
   useEffect(() => {
-    navigation.setOptions({
-      headerShown: true,
-      title: "Transaction History",
-    });
-  }, [navigation]);
+    fetchRecentTransactions();
+  }, []);
+
   return (
-    <View>
-      <Text>TransactionHistory</Text>
-    </View>
+    <SectionList
+      sections={recentTransactions}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <View style={{ backgroundColor: "white" }}>
+          <TransactionItem
+            data={{
+              category: item.category,
+              description: item.name,
+              date: item.createdAt,
+              amount: item.amount,
+              type: item.type,
+            }}
+            callback={() =>
+              router.push({
+                pathname: "/transactions/[id]",
+                params: { id: item.id },
+              })
+            }
+          />
+        </View>
+      )}
+      renderSectionHeader={({ section: { day } }) => (
+        <View style={styles.header}>
+          <Text style={styles.headerText}>{day}</Text>
+        </View>
+      )}
+      stickySectionHeadersEnabled={true}
+    />
   );
 };
 
-export default TransactionHistory;
+export default TransactionHistoryScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  item: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+});
